8183fe68b0c1316eec8bb7974504d8d169c6e654
Meredith Fry <meredithannfry@gmail.com>
2013-06-04 17:45:22 +1000

    Created Base Rails app
    Create as per cmd:
    >rails new mycommunity --skip-test-unit as we will be using doing TDD
    with Rspec test suite instead of TestUnit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mycommunity::Application.config.secret_token = '3329ff1528089a7571e16362dc7159e1ae68ea3bcb29f23c78b6b964d929cfa07c32ef2c73007cccd2f7808b61f83dbaac105af6e0477e6d3c7a46849cc7a901'
